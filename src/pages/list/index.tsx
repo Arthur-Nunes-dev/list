50d2487d50323e4beeb1b404468d164ddc884f7a
Arthur Nunes Silveira <arthurnunessilveiradev@gmail.com>
2025-04-12 17:36:34 -0300

    0.10 (beta)

---
+++ src/pages/list/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function List () {
+    return (
+        <Text>
+            Ola mundo lista!
+        </Text>
+    )
+}
